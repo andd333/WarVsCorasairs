942b44b4ecd4b5de0c09f8ae2f4b69aa174a08af
andre <andre@DESKTOP-C9GSKHB>
2025-04-17 15:00:42 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace WarVsCorasairs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Player1 = new System.Windows.Forms.PictureBox();
+            this.LeftMoveTimer = new System.Windows.Forms.Timer(this.components);
+            this.RightMoveTimer = new System.Windows.Forms.Timer(this.components);
+            this.Player2 = new System.Windows.Forms.PictureBox();
+            this.MoveBallsTimer = new System.Windows.Forms.Timer(this.components);
+            this.MoveCorsarsTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Player1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Player1
+            // 
+            this.Player1.BackColor = System.Drawing.Color.Transparent;
+            this.Player1.Image = ((System.Drawing.Image)(resources.GetObject("Player1.Image")));
+            this.Player1.Location = new System.Drawing.Point(359, 519);
+            this.Player1.Name = "Player1";
+            this.Player1.Size = new System.Drawing.Size(128, 128);
+            this.Player1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Player1.TabIndex = 1;
+            this.Player1.TabStop = false;
+            // 
+            // LeftMoveTimer
+            // 
+            this.LeftMoveTimer.Interval = 5;
+            this.LeftMoveTimer.Tick += new System.EventHandler(this.LeftMoveTimer_Tick);
+            // 
+            // RightMoveTimer
+            // 
+            this.RightMoveTimer.Interval = 5;
+            this.RightMoveTimer.Tick += new System.EventHandler(this.RightMoveTimer_Tick);
+            // 
+            // Player2
+            // 
+            this.Player2.BackColor = System.Drawing.Color.Transparent;
+            this.Player2.Image = ((System.Drawing.Image)(resources.GetObject("Player2.Image")));
+            this.Player2.Location = new System.Drawing.Point(359, 519);
+            this.Player2.Name = "Player2";
+            this.Player2.Size = new System.Drawing.Size(128, 128);
+            this.Player2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Player2.TabIndex = 2;
+            this.Player2.TabStop = false;
+            // 
+            // MoveBallsTimer
+            // 
+            this.MoveBallsTimer.Enabled = true;
+            this.MoveBallsTimer.Interval = 20;
+            this.MoveBallsTimer.Tick += new System.EventHandler(this.MoveBallsTimer_Tick);
+            // 
+            // MoveCorsarsTimer
+            // 
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(840, 650);
+            this.Controls.Add(this.Player2);
+            this.Controls.Add(this.Player1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "War with Corsairs";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress_1);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.Player1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox Player1;
+        private System.Windows.Forms.Timer LeftMoveTimer;
+        private System.Windows.Forms.Timer RightMoveTimer;
+        private System.Windows.Forms.PictureBox Player2;
+        private System.Windows.Forms.Timer MoveBallsTimer;
+        private System.Windows.Forms.Timer MoveCorsarsTimer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrayNotify;
+
+namespace WarVsCorasairs
+{
+    public partial class Form1 : Form
+    {
+        int playerSpeed;
+
+        private const int FIRE_COOLDOWN_MS = 10;
+        private const int MAX_BALLS = 1;
+        private List<Ball> activeBalls = new List<Ball>();
+        private Bitmap ballImage;
+        private DateTime lastFireTime = DateTime.MinValue;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true;
+            this.SetStyle(ControlStyles.OptimizedDoubleBuffer |
+                        ControlStyles.AllPaintingInWmPaint |
+                        ControlStyles.UserPaint, true);
+
+        }
+
+        private void Form1_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Escape)
+            {
+                this.Close();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            playerSpeed = 2;
+            Player1.Visible = true;
+            Player2.Visible = false;
+
+            ballImage = new Bitmap(@"sprites1\ball.png");
+            ballImage = new Bitmap(ballImage, new Size(20, 20));
+            ballImage.MakeTransparent();
+
+
+        }
+
+        private class Ball
+        {
+            public Point Position;
+            public bool IsActive;
+        }
+
+        private void Fire()
+        {
+            if ((DateTime.Now - lastFireTime).TotalMilliseconds < FIRE_COOLDOWN_MS)
+                return;
+
+            lastFireTime = DateTime.Now;
+
+            var inactiveBall = activeBalls.Find(b => !b.IsActive);
+            if (inactiveBall == null && activeBalls.Count < MAX_BALLS)
+            {
+                inactiveBall = new Ball();
+                activeBalls.Add(inactiveBall);
+            }
+
+            if (inactiveBall != null)
+            {
+                var player = Player1.Visible ? Player1 : Player2;
+                inactiveBall.Position = new Point(
+                    player.Left + (player.Width - ballImage.Width) / 2,
+                    player.Top - ballImage.Height / 3);
+                inactiveBall.IsActive = true;
+            }
+        }
+
+        private void MoveBallsTimer_Tick(object sender, EventArgs e)
+        {
+            for (int i = activeBalls.Count - 1; i >= 0; i--)
+            {
+                if (activeBalls[i].IsActive)
+                {
+                    activeBalls[i].Position.Y -= 9;
+                    if (activeBalls[i].Position.Y + ballImage.Height < 0)
+                    {
+                        activeBalls[i].IsActive = false;
+                    }
+                }
+            }
+            this.Invalidate();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            foreach (var ball in activeBalls)
+            {
+                if (ball.IsActive)
+                {
+                    e.Graphics.DrawImage(ballImage, ball.Position);
+                }
+            }
+        }
+
+        private void LeftMoveTimer_Tick(object sender, EventArgs e)
+        {
+            if (Player1.Left > 4)
+            {
+                Player1.Left -= playerSpeed;
+                Player1.Visible = true;
+                Player2.Visible = false;
+                Player2.Left = Player1.Left;
+            }
+        }
+
+        private void RightMoveTimer_Tick(object sender, EventArgs e)
+        {
+            if (Player2.Right < 630)
+            {
+                Player2.Left += playerSpeed;
+                Player2.Visible = true;
+                Player1.Visible = false;
+                Player1.Left = Player2.Left;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right || e.KeyCode == Keys.D)
+            {
+                RightMoveTimer.Start();
+            }
+            if (e.KeyCode == Keys.Left || e.KeyCode == Keys.A)
+            {
+                LeftMoveTimer.Start();
+            }
+            if (e.KeyCode == Keys.Space)
+            {
+                Fire();
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right || e.KeyCode == Keys.D)
+            {
+                RightMoveTimer.Stop();
+            }
+            if (e.KeyCode == Keys.Left || e.KeyCode == Keys.A)
+            {
+                LeftMoveTimer.Stop();
+            }
+        }
+    }
+}
